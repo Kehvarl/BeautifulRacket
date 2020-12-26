32e5091c970593639679a4f852e3c5280e21993f
Adam Corbett <kehvarl@gmail.com>
2020-12-25 18:44:15 -0700

    Stack Machine

--- stacker.rkt
+++ stacker.rkt
@@ -7,3 +7,20 @@
                        ,@src-datums)))
 
 (provide read-syntax)
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+  #'(#%module-begin
+     HANDLE-EXPR ...))
+
+(provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
