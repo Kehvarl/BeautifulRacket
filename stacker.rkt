2c72f3a01ebaf87575dc32fd37c24386d71dd076
Adam Corbett <kehvarl@gmail.com>
2020-12-25 18:39:23 -0700

    Reader

--- stacker.rkt
+++ stacker.rkt
@@ -1,2 +1,9 @@
 #lang br/quicklang
-42
+
+(define (read-syntax path port)
+  (define src-lines (port->lines port))
+  (define src-datums (format-datums '(handle ~a) src-lines))
+  (datum->syntax #f `(module stacker-mod "stacker.rkt"
+                       ,@src-datums)))
+
+(provide read-syntax)
