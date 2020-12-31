abc1ce423763bdc3eaf4bb4557dcb7c144a05ccc
Adam Corbett <kehvarl@gmail.com>
2020-12-30 19:13:40 -0700

    needed features

--- bf/expander.rkt
+++ bf/expander.rkt
@@ -8,3 +8,32 @@
 (define-macro (bf-program OP-OR-LOOP-ARG ...)
   #'(void OP-OR-LOOP-ARG ...))
 (provide bf-program)
+
+(define-macro (bf-loop "[" OP-OR-LOOP-ARG ... "]")
+  #'(until (zero? (current-byte))
+           OP-OR-LOOP-ARG ...))
+(provide bf-loop)
+
+(define-macro-cases bf-op
+  [(bf-op ">") #'(bf-next)]
+  [(bf-op "<") #'(bf-prev)]
+  [(bf-op "+") #'(bf-inc)]
+  [(bf-op "-") #'(bf-dec)]
+  [(bf-op ".") #'(bf-out)]
+  [(bf-op ",") #'(bf-in)])
+(provide bf-op)
+
+(define arr (make-vector 30000 0))
+(define pty 0)
+
+(define (current-byte) (vector-ref arr ptr))
+(define (set-current-byte! val) (vector-set! arr ptr val))
+
+(define (bf-next) (set! ptr (add1 ptr)))
+(define (bf-prev) (set! ptr (sub1 ptr)))
+
+(define (bf-inc) (set-current-byte! (add1 current-byte)))
+(define (bf-dec) (set-current-byte! (sub1 current-byte)))
+
+(define (bf-out) (write-byte (current-byte)))
+(define (bf-in) (set-current-byte! (read-byte)))
