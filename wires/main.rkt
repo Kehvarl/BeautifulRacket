648133230fdac750d4535aa2f9d7c023d8f31ab6
Adam Corbett <kehvarl@gmail.com>
2021-01-04 19:50:58 -0700

    flesh out wire macro

--- wires/main.rkt
+++ wires/main.rkt
@@ -17,8 +17,12 @@
 
 
 (define-macro-cases wire
-  [(wire ARG -> ID) ...]
-  [(wire OP ARG -> ID) ...]
-  [(wire ARG1 OP ARG2 -> ID) ...]
-  [else ...])
+  [(wire ARG -> ID)
+   #'(define/display (ID) (val ARG))]
+  [(wire OP ARG -> ID)
+   #'(wire (OP (val ARG)) -> ID)]
+  [(wire ARG1 OP ARG2 -> ID)
+   #'(wire (OP (val ARG1) (val ARG2)) -> ID)]
+  [else `'(void)])
+(provide wire)
   
