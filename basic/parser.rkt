f8887c75dea12490fac4366ef2278b26a945fd29
Adam Corbett <kehvarl@gmail.com>
2021-01-07 18:41:12 -0700

    REM is easy.  Prep for statements

--- basic/parser.rkt
+++ basic/parser.rkt
@@ -2,4 +2,8 @@
 b-program   : [b-line] (NEWLINE [b-line])*
 b-line      : b-line-num [b-statement] (":" [b-statement])*
 b-line-num  : INTEGER
-b-statement : 
+b-statement : b-end | b-print | b-goto
+b-rem       : REM
+b-end       :
+b-print     :
+b-goto      :
