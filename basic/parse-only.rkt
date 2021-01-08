ea03946ba659c1bea1e54aef5db088219c7f17ab
Adam Corbett <kehvarl@gmail.com>
2021-01-07 20:24:28 -0700

    parse-only mode for testing

--- basic/parse-only.rkt
+++ basic/parse-only.rkt
@@ -7,3 +7,8 @@
    #`(module basic-parser-mod basic/parse-only
        #,parse-tree)))
 (module+ reader (provide read-syntax))
+
+(define-macro (parse-only-mb PARSE-TREE)
+  #'(#%module-begin
+     'PARSE-TREE))
+(provide (rename-out [parse-only-mb #%module-begin]))
