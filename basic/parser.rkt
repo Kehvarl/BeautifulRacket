101e3dc3eb09a509d8238431746395bdb51b1dd1
Adam Corbett <kehvarl@gmail.com>
2021-01-07 18:40:12 -0700

    more lenient parsing of lines

--- basic/parser.rkt
+++ basic/parser.rkt
@@ -1,5 +1,5 @@
 #lang brag
 b-program   : [b-line] (NEWLINE [b-line])*
-b-line      : b-line-num [b-statement (":" b-statement)*]
+b-line      : b-line-num [b-statement] (":" [b-statement])*
 b-line-num  : INTEGER
 b-statement : 
