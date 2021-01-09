de063ec2003c6e50f7e7d589f2bdfdf10cff13da
Adam Corbett <kehvarl@gmail.com>
2021-01-09 15:45:24 -0700

    include source locations for debugging

--- basic/expander.rkt
+++ basic/expander.rkt
@@ -1,5 +1,8 @@
 #lang br/quicklang
 
 (define-macro (b-line NUM STATEMENT ...)
-  (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM)])
-    #'(define (LINE-NUM) (void) STATEMENT ...)))
+  (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM
+                                      #:source #'NUM)])
+    (syntax/lox caller-stx
+                (define (LINE-NUM) (void) STATEMENT ...))))
+
