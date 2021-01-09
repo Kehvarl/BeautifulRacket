032b2974e00edce7862742ebbd9a4c9a1bea5b6d
Adam Corbett <kehvarl@gmail.com>
2021-01-09 15:45:51 -0700

    typo in syntax/loc

--- basic/expander.rkt
+++ basic/expander.rkt
@@ -3,6 +3,6 @@
 (define-macro (b-line NUM STATEMENT ...)
   (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM
                                       #:source #'NUM)])
-    (syntax/lox caller-stx
+    (syntax/loc caller-stx
                 (define (LINE-NUM) (void) STATEMENT ...))))
 
