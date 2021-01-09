1c086f2600b823f7e3339d6072ce9fe3ed6f0fcd
Adam Corbett <kehvarl@gmail.com>
2021-01-09 15:42:31 -0700

    can't have functions with numeric names.

--- basic/expander.rkt
+++ basic/expander.rkt
@@ -1,4 +1,5 @@
 #lang br/quicklang
 
 (define-macro (b-line NUM STATEMENT ...)
-  #'(define (NUM) (void) STATEMENT ...))
+  (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM)])
+    #'(define (LINE-NUM) (void) STATEMENT ...)))
