345e767e40472b9e391339c9b7aa621a57239aa2
Adam Corbett <kehvarl@gmail.com>
2021-01-10 14:07:35 -0700

    debug before moving on.

--- basic/elements.rkt
+++ basic/elements.rkt
@@ -5,9 +5,3 @@
  (all-from-out "line.rkt" "go.rkt"
                "expr.rkt" "misc.rkt"))
 
-#lang br
-(require "line.rkt" "go.rkt"
-         "expr.rkt" "misc.rkt")
-(provide
- (all-from-out "line.rkt" "go.rkt"
-               "expr.rkt" "misc.rkt"))
