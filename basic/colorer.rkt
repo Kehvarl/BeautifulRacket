3bb74fe49da1b909e350106633abc6f6cbf26b9f
Adam Corbett <kehvarl@gmail.com>
2021-01-10 14:13:51 -0700

    catch errors in lexing so we can colorize them

--- basic/colorer.rkt
+++ basic/colorer.rkt
@@ -3,6 +3,12 @@
 (provide basic-colorer)
 
 (define (basic-colorer port)
-  (define srcloc-tok (basic-lexedr port))
+  (define (handle-lexer-error excn)
+    (define excn-srclocs (exn:fail:read-srclocs excn))
+    (srcloc-token (token 'ERROR) (car excn-srclocs)))
+      
+  (define srcloc-tok
+    (with-handlers ([exn:fail:read? handle-lexer-error])
+      (basix-lexer port)))
   ...)
 
