e93f9dc87f0acf99d7b6cb243e20f27db1ab1b3c
Adam Corbett <kehvarl@gmail.com>
2021-01-12 20:07:48 -0700

    hack b-sums to just pass single items through so strings don't get eaten

--- basic/expr.rkt
+++ basic/expr.rkt
@@ -1,6 +1,10 @@
 #lang br
 (provide b-sum b-expr)
 
-(define (b-sum . nums) (apply + nums))
+(define (b-sum . nums)
+  (if (= 1 (length nums))
+      (car nums)
+      (apply + nums)))
+
 (define (b-expr expr)
   (if (integer? expr) (inexact->exact expr) expr))
