dfa044485ede8bed416e2812c81d4efe0f83433c
Adam Corbett <kehvarl@gmail.com>
2021-01-16 13:09:29 -0700

    def functions in the lexer

--- basic/lexer.rkt
+++ basic/lexer.rkt
@@ -10,7 +10,8 @@
                                        "if" "then" "else"
                                        "<" ">" "and" "or" "not"
                                        "gosub" "return"
-                                       "for" "to" "step" "next"))
+                                       "for" "to" "step" "next"
+                                       "def" ","))
 
 (define (basic-lexer port)
   (define the-lexer
