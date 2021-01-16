8744502172bf5c5bbdbed0f560363366c4c519f3
Adam Corbett <kehvarl@gmail.com>
2021-01-15 19:40:17 -0700

    stack and stack handlers for gosub/return

--- basic/go.rkt
+++ basic/go.rkt
@@ -1,6 +1,6 @@
 #lang br
 (require "struct.rkt" "line.rkt")
-(provide b-end b-goto)
+(provide b-end b-goto b-gosub b-return)
 
 
 (define (b-end)
@@ -8,3 +8,16 @@
 
 (define (b-goto expr)
   (raise (change-line-signal expr)))
+
+(define return-ccs empty)
+
+(define (b-gosub num-expr)
+  (let/cc here-cc
+    (push! return-ccs here-cc)
+    (b-goto num-expr)))
+
+(define (b-return)
+  (when (empty? return-ccs)
+    (raise-line-error "return without gosub"))
+  (define top-cc (pop! return-ccs))
+  (top-cc (void)))
